382a1a88504ddd9090782cbf4a4c60b3dfbd8a9e
luoweida <3275365883@qq.com>
2023-04-12 10:22:06 +0800

    引入element ui组件库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Row, Col, Button, Select } from 'element-ui';
 
 Vue.config.productionTip = false
+
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   render: h => h(App),
